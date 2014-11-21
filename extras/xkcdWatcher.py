56341c9f86858298541129f4a5ee259c36b57c3b
Chris McCann <testflyjets@gmail.com>
2014-11-20 23:43:27 -0800

    Cleans up debugging changes

--- extras/xkcdWatcher.py
+++ extras/xkcdWatcher.py
@@ -4,9 +4,9 @@ import lxml.html
 import time
 
 CHECK_DELAY = 1 * 60 * 60  # 1 hour * 60 min/hr * 60 seconds/min = 3600 secs = 1 hr
-# CHECK_DELAY = 10 ## 10 seconds in testing mode 
+## CHECK_DELAY = 10 ## 10 seconds in testing mode 
 
-sp = serial.Serial("/dev/tty.usbserial-AM01QY9L", 38400, timeout = 2)
+sp = serial.Serial("/dev/ttyUSB0", 38400, timeout = 2)
 sp.flush()  # clear out whatever junk is in the serial buffer
 
 def whichXKCD():
@@ -15,7 +15,7 @@ def whichXKCD():
     comicDiv = xkcd.getroot().get_element_by_id("comic")
     comicLink = comicDiv[0].attrib['src']
     try:    
-        comicName = comicLink.split("/")[4]   
+        comicName = comicLink.split("/")[4]   ## Get the name of the comic PNG file
     except ValueError:  ## it wasn't able to convert to integer
         raise RuntimeError("can't parse the link: %s" % comicLink)
     return comicName
